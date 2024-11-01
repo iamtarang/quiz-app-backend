8b623cd47d9f1180dc1a977b57c430f2de20cef3
Tarang Lakhmapurkar <tarangjlakh@gmail.com>
2024-11-01 12:06:13 +0530

    testing changes

--- routes/admin/facultyRoute.js
+++ routes/admin/facultyRoute.js
@@ -6,22 +6,22 @@ const facultyRouter = express.Router();
 //! Faculty Operations
 
 //? get all facultys
-facultyRouter.get('/get', facultyController.getAllFaculty);
+facultyRouter.post('/get', facultyController.getAllFaculty);
 
 //? create facultys
 facultyRouter.post('/create', facultyController.createFaculty);
 
 //? update facultys
-facultyRouter.patch('/update/:id', facultyController.updateFaculty);
+facultyRouter.post('/update/:id', facultyController.updateFaculty);
 
 //? enable facultys
-facultyRouter.patch('/enable/:id', facultyController.enableFaculty);
+facultyRouter.post('/enable/:id', facultyController.enableFaculty);
 
 //? disable facultys
-facultyRouter.patch('/disable/:id', facultyController.disableFaculty);
+facultyRouter.post('/disable/:id', facultyController.disableFaculty);
 
 //? delete facultys
-facultyRouter.patch('/delete/:id', facultyController.removeFaculty);
+facultyRouter.post('/delete/:id', facultyController.removeFaculty);
 
 //? destroy facultys
 facultyRouter.delete('/destroy/:id', facultyController.destroyFaculty);
